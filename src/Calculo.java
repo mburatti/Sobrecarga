fd4ab55b7a9cc5d54e1a1185a5837afdad128534
Michael Christian Buratti <mburatti@users.noreply.github.com>
2019-11-25 10:41:54 -0500

    Initial commit

---
+++ src/Calculo.java
@@ -0,0 +1,10 @@
+
+public class Calculo {
+	public int Soma(int n1, int n2) {
+		return n1+n2;
+	}
+	
+	public double Soma(double n1, double n2) {
+		return n1+n2;
+	}
+}
---
+++ src/SobreCarga.java
@@ -0,0 +1,12 @@
+
+public class SobreCarga {
+
+	public static void main(String[] args) {
+		Calculo calculo = new Calculo();
+
+		System.out.println(calculo.Soma(1, 2));
+        
+        System.out.println(calculo.Soma(1.3, 2.3));
+	}
+
+}
